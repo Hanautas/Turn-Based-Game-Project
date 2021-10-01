9ee6ac676fb175202167c5b386736af4543dacef
catsmeow4753 <77382504+catsmeow4753@users.noreply.github.com>
2021-10-01 20:04:41 +0200

    Final

---
+++ Turn-Based Game/Assets/Scripts/ShadowEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ShadowEffect : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(-0.1f, -0.1f);
+    public Material material;
+
+    public GameObject shadow;
+
+    void Start()
+    {
+        shadow = new GameObject("Shadow");
+        shadow.transform.parent = transform;
+        shadow.transform.localPosition = offset;
+        //shadow.transform.localPosition = Quaternion.identity;
+
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer sr = shadow.GetComponent<SpriteRenderer>();
+
+        sr.sprite = renderer.sprite;
+        sr.material = material;
+        sr.sortingLayerName = renderer.sortingLayerName;
+        sr.sortingOrder = renderer.sortingOrder - 1;
+    }
+
+    void LateUpdate()
+    {
+        shadow.transform.localPosition = offset;
+    }
+}
